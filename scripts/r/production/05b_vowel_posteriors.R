6cf7b9fc1564ac75a0f2d849b32e670839eb97db
jvcasillas <joseph.casillas@rutgers.edu>
2019-09-10 22:31:28 -0400

    vowel posteriors

--- scripts/r/production/05b_vowel_posteriors.R
+++ scripts/r/production/05b_vowel_posteriors.R
@@ -9,7 +9,7 @@
 
 # Source files ----------------------------------------------------------------
 
-source(here::here("scripts", "r", "production", "04a_mono_analysis.R"))
+source(here::here("scripts", "r", "production", "05a_vowel_analysis.R"))
 
 # -----------------------------------------------------------------------------
 
@@ -19,32 +19,16 @@ source(here::here("scripts", "r", "production", "04a_mono_analysis.R"))
 # Combined posterior ----------------------------------------------------------
 
 bind_rows(
-  posterior_samples(mod_coronals_vot_mono_full) %>%
+  posterior_samples(mod_f1_mono_full) %>%
     select(starts_with("b_")) %>%
-    mutate(metric = "VOT"),
-  posterior_samples(mod_coronals_ri_mono_full) %>%
+    mutate(metric = "f1"),
+  posterior_samples(mod_f2_mono_full) %>%
     select(starts_with("b_")) %>%
-    mutate(metric = "RI"),
-  posterior_samples(mod_coronals_cog_mono_full) %>%
-    select(starts_with("b_")) %>%
-    mutate(metric = "COG"),
-  posterior_samples(mod_coronals_sd_mono_full) %>%
-    select(starts_with("b_")) %>%
-    mutate(metric = "SD"),
-  posterior_samples(mod_coronals_sk_mono_full) %>%
-    select(starts_with("b_")) %>%
-    mutate(metric = "SK"),
-  posterior_samples(mod_coronals_kt_mono_full) %>%
-    select(starts_with("b_")) %>%
-    mutate(metric = "KT")) %>%
+    mutate(metric = "f2")) %>%
   gather(parameters, estimate, -metric) %>%
-  mutate(metric = fct_relevel(metric, "VOT", "RI"),
-         parameters = fct_relevel(parameters,
-          "b_group_sum:phon_sum:stress_sum", "b_phon_sum:stress_sum",
-          "b_group_sum:stress_sum", "b_group_sum:phon_sum", "b_rep_n",
-          "b_stress_sum", "b_phon_sum", "b_group_sum", "b_Intercept")) %>%
-  saveRDS(., here("data", "models", "posterior_mono.rds"))
-
+  mutate(parameters = fct_relevel(parameters,
+         "b_rep_n", "b_phon_sum", "b_language_sum", "b_Intercept")) %>%
+  saveRDS(., here("data", "models", "posterior_vowels.rds"))
 
 # -----------------------------------------------------------------------------
 
@@ -55,55 +39,22 @@ bind_rows(
 
 # Key info:
 # phon_sum = if_else(phon == "d", 1, -1),
-# group_sum = if_else(group == "NEN", 1, -1),
 # language_sum = if_else(language == "english", 1, -1),
-# stress_sum = if_else(stress == "stressed", 1, -1))
 
-posterior_mono_adj <-
-  bind_rows(
-    posterior_samples(mod_coronals_vot_mono_full) %>%
+bind_cols(
+  posterior_samples(mod_f1_mono_full) %>%
     select(starts_with("b_")) %>%
-    mono_prep %>%
-    gather(language, val) %>%
-    separate(language, into = c("language", "phon", "stress"),
-             sep = "_", remove = T) %>%
-    mutate(metric = "vot"),
-  posterior_samples(mod_coronals_ri_mono_full) %>%
+    vowel_prep %>%
+    gather(language, f1) %>%
+    separate(language, into = c("language", "phon"),
+             sep = "_", remove = T),
+  posterior_samples(mod_f2_mono_full) %>%
     select(starts_with("b_")) %>%
-    mono_prep %>%
-    gather(language, val) %>%
-    separate(language, into = c("language", "phon", "stress"),
-             sep = "_", remove = T) %>%
-    mutate(metric = "ri"),
-  posterior_samples(mod_coronals_cog_mono_full) %>%
-    select(starts_with("b_")) %>%
-    mono_prep %>%
-    gather(language, val) %>%
-    separate(language, into = c("language", "phon", "stress"),
-             sep = "_", remove = T) %>%
-    mutate(metric = "cog"),
-  posterior_samples(mod_coronals_sd_mono_full) %>%
-    select(starts_with("b_")) %>%
-    mono_prep %>%
-    gather(language, val) %>%
-    separate(language, into = c("language", "phon", "stress"),
-             sep = "_", remove = T) %>%
-    mutate(metric = "sd"),
-  posterior_samples(mod_coronals_sk_mono_full) %>%
-    select(starts_with("b_")) %>%
-    mono_prep %>%
-    gather(language, val) %>%
-    separate(language, into = c("language", "phon", "stress"),
-             sep = "_", remove = T) %>%
-    mutate(metric = "sk"),
-  posterior_samples(mod_coronals_kt_mono_full) %>%
-    select(starts_with("b_")) %>%
-    mono_prep %>%
-    gather(language, val) %>%
-    separate(language, into = c("language", "phon", "stress"),
-             sep = "_", remove = T) %>%
-    mutate(metric = "kt")
-  ) %>%
-  saveRDS(., here("data", "models", "posterior_mono_adj.rds"))
+    vowel_prep %>%
+    gather(language, f2) %>%
+    separate(language, into = c("language", "phon"),
+             sep = "_", remove = T)) %>%
+  select(-language1, -phon1) %>%
+  saveRDS(., here("data", "models", "posterior_vowels_adj.rds"))
 
 # -----------------------------------------------------------------------------
