a4abf4ef6b5266e6fe9ba1f4cc865ebe70949009
jvcasillas <joseph.casillas@rutgers.edu>
2019-09-11 14:46:25 -0400

    update bilingual posteriors

--- scripts/r/production/06b_bi_posteriors.R
+++ scripts/r/production/06b_bi_posteriors.R
@@ -40,9 +40,8 @@ bind_rows(
   gather(parameters, estimate, -metric) %>%
   mutate(metric = fct_relevel(metric, "VOT", "RI"),
          parameters = fct_relevel(parameters,
-          "b_language_sum:phon_sum:stress_sum", "b_phon_sum:stress_sum",
-          "b_language_sum:stress_sum", "b_language_sum:phon_sum", "b_rep_n",
-          "b_stress_sum", "b_phon_sum", "b_language_sum", "b_Intercept")) %>%
+          "b_language_sum:phon_sum", "b_rep_n", "b_f2_std", "b_f1_std",
+          "b_phon_sum", "b_language_sum", "b_Intercept")) %>%
   saveRDS(., here("data", "models", "posterior_bi.rds"))
 
 # -----------------------------------------------------------------------------
@@ -65,42 +64,42 @@ posterior_bi_adj <-
     select(starts_with("b_")) %>%
     bi_prep %>%
     gather(language, val) %>%
-    separate(language, into = c("language", "phon", "stress"),
+    separate(language, into = c("language", "phon"),
              sep = "_", remove = T) %>%
     mutate(metric = "vot"),
   posterior_samples(mod_coronals_ri_bi_full) %>%
     select(starts_with("b_")) %>%
     bi_prep %>%
     gather(language, val) %>%
-    separate(language, into = c("language", "phon", "stress"),
+    separate(language, into = c("language", "phon"),
              sep = "_", remove = T) %>%
     mutate(metric = "ri"),
   posterior_samples(mod_coronals_cog_bi_full) %>%
     select(starts_with("b_")) %>%
     bi_prep %>%
     gather(language, val) %>%
-    separate(language, into = c("language", "phon", "stress"),
+    separate(language, into = c("language", "phon"),
              sep = "_", remove = T) %>%
     mutate(metric = "cog"),
   posterior_samples(mod_coronals_sd_bi_full) %>%
     select(starts_with("b_")) %>%
     bi_prep %>%
     gather(language, val) %>%
-    separate(language, into = c("language", "phon", "stress"),
+    separate(language, into = c("language", "phon"),
              sep = "_", remove = T) %>%
     mutate(metric = "sd"),
   posterior_samples(mod_coronals_sk_bi_full) %>%
     select(starts_with("b_")) %>%
     bi_prep %>%
     gather(language, val) %>%
-    separate(language, into = c("language", "phon", "stress"),
+    separate(language, into = c("language", "phon"),
              sep = "_", remove = T) %>%
     mutate(metric = "sk"),
   posterior_samples(mod_coronals_kt_bi_full) %>%
     select(starts_with("b_")) %>%
     bi_prep %>%
     gather(language, val) %>%
-    separate(language, into = c("language", "phon", "stress"),
+    separate(language, into = c("language", "phon"),
              sep = "_", remove = T) %>%
     mutate(metric = "kt")
   ) %>%
