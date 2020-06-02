1413e427c725f5cde375efe6e98924976bb4fd0b
jvcasillas <joseph.casillas@rutgers.edu>
2020-06-02 17:14:24 -0400

    simplify posterior calculations to include multivariate vowel model

--- scripts/r/production/04b_vowel_posteriors.R
+++ scripts/r/production/04b_vowel_posteriors.R
@@ -18,16 +18,18 @@ source(here::here("scripts", "r", "production", "04a_vowel_analysis.R"))
 
 # Combined posterior ----------------------------------------------------------
 
-bind_rows(
-  posterior_samples(mod_f1_mono_full) %>%
-    select(starts_with("b_")) %>%
-    mutate(metric = "F1"),
-  posterior_samples(mod_f2_mono_full) %>%
-    select(starts_with("b_")) %>%
-    mutate(metric = "F2")) %>%
-  gather(parameters, estimate, -metric) %>%
-  mutate(parameters = fct_relevel(parameters,
-         "b_rep_n", "b_phon_sum", "b_language_sum", "b_Intercept")) %>%
+posterior_samples(mod_f1f2_mv_mono_full) %>%
+  select(starts_with("b_")) %>%
+  pivot_longer(cols = everything(), values_to = "estimate") %>%
+  separate(col = name, into = c("pt1", "metric", "pt2", "pt3"), sep = "_") %>%
+  unite(col = "parameters", pt1, pt2, pt3, sep = "_") %>%
+  mutate(metric = str_replace(metric, "std", ""),
+         metric = str_replace(metric, "f", "F"),
+         parameters = str_replace(parameters, "_NA", ""),
+         parameters = fct_relevel(parameters,
+          "b_rep_n", "b_phon_sum", "b_language_sum", "b_Intercept")) %>%
+  select(metric, parameters, estimate) %>%
+  arrange(metric) %>%
   saveRDS(., here("data", "models", "posterior_vowels.rds"))
 
 # -----------------------------------------------------------------------------
@@ -41,20 +43,15 @@ bind_rows(
 # phon_sum = if_else(phon == "d", 1, -1),
 # language_sum = if_else(language == "english", 1, -1),
 
-bind_cols(
-  posterior_samples(mod_f1_mono_full) %>%
-    select(starts_with("b_")) %>%
-    vowel_prep %>%
-    gather(language, f1) %>%
-    separate(language, into = c("language", "phon"),
-             sep = "_", remove = T),
-  posterior_samples(mod_f2_mono_full) %>%
-    select(starts_with("b_")) %>%
-    vowel_prep %>%
-    gather(language, f2) %>%
-    separate(language, into = c("language", "phon"),
-             sep = "_", remove = T)) %>%
-  select(-language1, -phon1) %>%
+posterior_samples(mod_f1f2_mv_mono_full) %>%
+  select(starts_with("b_")) %>%
+  vowel_prep %>%
+  pivot_longer(cols = everything(), names_to = "metric", values_to = "val") %>%
+  separate(metric, into = c("metric", "language", "phon"), sep = "_") %>%
+  arrange(metric, language, phon) %>%
+  group_by(metric, language, phon) %>%
+  mutate(draw = seq_along(phon)) %>%
+  pivot_wider(names_from = "metric", values_from = "val") %>%
   saveRDS(., here("data", "models", "posterior_vowels_adj.rds"))
 
 # -----------------------------------------------------------------------------
