e3881c6dc3c4e6945779531109a3de8440c6443e
jvcasillas <joseph.casillas@rutgers.edu>
2020-06-02 17:13:44 -0400

    add multivariate model

--- scripts/r/production/04a_vowel_analysis.R
+++ scripts/r/production/04a_vowel_analysis.R
@@ -26,8 +26,11 @@ coronals_vowels <- coronals %>%
   filter(., group %in% c("NEN", "NSP"), is.na(label)) %>%
   mutate(., language_sum = if_else(language == "english", 1, -1),
             phon_sum = if_else(phon == "d", 1, -1),
-            f1_std = (f1_mp - mean(f1_mp)) / sd(f1_mp),
-            f2_std = (f2_mp - mean(f2_mp)) / sd(f2_mp))
+            f1_std = (f1_cent - mean(f1_cent)) / sd(f1_cent),
+            f2_std = (f2_cent - mean(f2_cent)) / sd(f2_cent),
+            mean_f1 = mean(f1_std), mean_f2 = mean(f2_std),
+            euc_dist = sqrt((0 - 0)^2 + (f1_std - f2_std)^2))
+
 
 # Use all available cores for parallel computing
 options(mc.cores = parallel::detectCores())
@@ -44,33 +47,23 @@ priors <- c(
 
 
 
-# Fit models ------------------------------------------------------------------
 
-# F1
-mod_f1_mono_full <- brm(
-  formula = f1_std ~ 1 + language_sum + phon_sum + rep_n +
-    (1 + phon_sum + rep_n | id) +
-    (1 + rep_n | item),
-  prior = priors,
-  warmup = 1000, iter = 4000, chains = 4, cores = parallel::detectCores(),
-  family = gaussian(),
-  control = list(adapt_delta = 0.999, max_treedepth = 15),
-  data = coronals_vowels,
-  file = here("data", "models", "mod_f1_mono_full")
+# F1/F2 multivariate model  ---------------------------------------------------
+
+mv_vowel_model <- bf(
+  mvbind(f1_std, f2_std) ~ 1 + language_sum + phon_sum + rep_n +
+        (1 + phon_sum + rep_n |p| id) +
+        (1 + rep_n |q| item)
 )
 
-
-# F2
-mod_f2_mono_full <- brm(
-  formula = f2_std ~ 1 + language_sum + phon_sum + rep_n +
-    (1 + phon_sum + rep_n | id) +
-    (1 + rep_n | item),
+mod_f1f2_mv_mono_full <- brm(
+  formula = mv_vowel_model,
   prior = priors,
   warmup = 1000, iter = 4000, chains = 4, cores = parallel::detectCores(),
   family = gaussian(),
   control = list(adapt_delta = 0.999, max_treedepth = 15),
   data = coronals_vowels,
-  file = here("data", "models", "mod_f2_mono_full")
+  file = here("data", "models", "mod_f1f2_mv_mono_full")
 )
 
 # -----------------------------------------------------------------------------
