2eaa13873195e45fcc29cc1a251088e39d8b0c72
jvcasillas <joseph.casillas@rutgers.edu>
2020-06-02 17:17:22 -0400

    old changes I forgot to add along time ago

--- scripts/r/production/99_small_data.R
+++ scripts/r/production/99_small_data.R
@@ -63,23 +63,63 @@ f2_mpe  <- all_models$mod_f2_mono_full %>% get_mpe()
 mono_vot_rope <- all_models$mod_coronals_vot_mono %>% get_rope()
 mono_vot_mpe  <- all_models$mod_coronals_vot_mono %>% get_mpe()
 mono_comp_d_t <- glue::glue("(&beta; = {mean}; CI = {hdi}; ROPE = {rope}; MPE = {mpe})",
-                 mean = pull(all_models$post_hoc_analyses[1, 2]),
-                 hdi = pull(all_models$post_hoc_analyses[1, 3]),
-                 rope = pull(all_models$post_hoc_analyses[1, 4]),
-                 mpe = pull(all_models$post_hoc_analyses[1, 5]))
-
-
-
-
+                   mean = pull(all_models$post_hoc_analyses[1, 2]),
+                   hdi = pull(all_models$post_hoc_analyses[1, 3]),
+                   rope = pull(all_models$post_hoc_analyses[1, 4]),
+                   mpe = pull(all_models$post_hoc_analyses[1, 5]))
 # RI
+mono_ri_rope  <- all_models$mod_coronals_ri_mono %>% get_rope()
+mono_ri_mpe   <- all_models$mod_coronals_ri_mono %>% get_mpe()
+# COG
+mono_cog_rope <- all_models$mod_coronals_cog_mono %>% get_rope()
+mono_cog_mpe  <- all_models$mod_coronals_cog_mono %>% get_mpe()
+# KT
+mono_kt_rope  <- all_models$mod_coronals_kt_mono %>% get_rope()
+mono_kt_mpe   <- all_models$mod_coronals_kt_mono %>% get_mpe()
+# SD
+mono_sd_rope  <- all_models$mod_coronals_sd_mono %>% get_rope()
+mono_sd_mpe   <- all_models$mod_coronals_sd_mono %>% get_mpe()
+# SK
+mono_sk_rope  <- all_models$mod_coronals_sk_mono %>% get_rope()
+mono_sk_mpe   <- all_models$mod_coronals_sk_mono %>% get_mpe()
 
 # -----------------------------------------------------------------------------
+
+
+
 
 
 # Bilingual analyses ----------------------------------------------------------
 
+# VOT
+bi_vot_rope <- all_models$mod_coronals_vot_bi %>% get_rope()
+bi_vot_mpe  <- all_models$mod_coronals_vot_bi %>% get_mpe()
+bi_comp_d_t <- glue::glue("(&beta; = {mean}; CI = {hdi}; ROPE = {rope}; MPE = {mpe})",
+                 mean = pull(all_models$post_hoc_analyses[2, 2]),
+                 hdi = pull(all_models$post_hoc_analyses[2, 3]),
+                 rope = pull(all_models$post_hoc_analyses[2, 4]),
+                 mpe = pull(all_models$post_hoc_analyses[2, 5]))
+# RI
+bi_ri_rope  <- all_models$mod_coronals_ri_bi %>% get_rope()
+bi_ri_mpe   <- all_models$mod_coronals_ri_bi %>% get_mpe()
+# COG
+bi_cog_rope <- all_models$mod_coronals_cog_bi %>% get_rope()
+bi_cog_mpe  <- all_models$mod_coronals_cog_bi %>% get_mpe()
+# KT
+bi_kt_rope  <- all_models$mod_coronals_kt_bi %>% get_rope()
+bi_kt_mpe   <- all_models$mod_coronals_kt_bi %>% get_mpe()
+# SD
+bi_sd_rope  <- all_models$mod_coronals_sd_bi %>% get_rope()
+bi_sd_mpe   <- all_models$mod_coronals_sd_bi %>% get_mpe()
+# SK
+bi_sk_rope  <- all_models$mod_coronals_sk_bi %>% get_rope()
+bi_sk_mpe   <- all_models$mod_coronals_sk_bi %>% get_mpe()
 
 # -----------------------------------------------------------------------------
+
+
+
+
 
 
 # POA analyses ----------------------------------------------------------------
